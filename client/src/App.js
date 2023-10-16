341fda30038b8b13117b2859fba487d2d03c3d0a
B-atuti <brian.atuti@student.moringaschool.com>
2023-10-16 10:02:34 +0300

    done

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Home from "./components/Home";
+import HeroPowerForm from "./components/HeroPowerForm";
+import Power from "./components/Power";
+import PowerEditForm from "./components/PowerEditForm";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <Switch>
+          <Route exact path="/hero_powers/new">
+            <HeroPowerForm />
+          </Route>
+          <Route exact path="/powers/:id/edit">
+            <PowerEditForm />
+          </Route>
+          <Route exact path="/powers/:id">
+            <Power />
+          </Route>
+          <Route exact path="/heroes/:id">
+            <Hero />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+}
+
+
+export default App;
